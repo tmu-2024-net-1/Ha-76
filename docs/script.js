cc75e58948cd74bf32b1ecae4ea8624cb815141b
Ha-76 <24244001@ed.tmu.ac.jp>
2024-07-09 11:32:17 +0900

    JS

---
+++ docs/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('scroll', function () {
+    const zones = document.querySelectorAll('.zone');
+    zones.forEach(zone => {
+      const rect = zone.getBoundingClientRect();
+      if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+        zone.classList.add('visible');
+      } else {
+        zone.classList.remove('visible');
+      }
+    });
+  });
+  
