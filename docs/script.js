d724efd841082a1aea782828fe275e0b96b3d8aa
Ha-76 <24244001@ed.tmu.ac.jp>
2024-07-09 15:24:38 +0900

    reset

--- docs/script.js
+++ docs/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  window.scrollTo({ top: document.body.scrollHeight, behavior: 'instant' });
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'instant' });
     const zones = document.querySelectorAll('.zone');
     zones.forEach(zone => {
       const rect = zone.getBoundingClientRect();
@@ -9,21 +9,5 @@ document.addEventListener('DOMContentLoaded', function () {
         zone.classList.remove('visible');
       }
     });
-    function checkVisibility() {
-      const zones = document.querySelectorAll('.zone');
-      zones.forEach(zone => {
-        const rect = zone.getBoundingClientRect();
-        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
-          zone.classList.add('visible');
-        } else {
-          zone.classList.remove('visible');
-        }
-      });
-    }
-    
-    // スクロールイベントで可視性をチェック
-    document.addEventListener('scroll', checkVisibility);
-    // 初期表示時にも可視性をチェック
-    checkVisibility();
   });
   
